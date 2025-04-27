acf0f81d508c9c4479a2ff9eda8177869600fb38
Ken Karlson <javarecords@mail.ru>
2025-04-28 02:54:56 +0300

    :(

---
+++ src/store/modules/pleasure.js
@@ -0,0 +1,55 @@
+const pleasure = {
+  namespaced: true,
+  state: {
+    goods: [
+      {
+        id: 0,
+        name: '',
+        price: 10.73,
+        image: 'coffee-1.jpg',
+        country: 'Russia',
+      },
+      {
+        id: 1,
+        name: '',
+        price: 10.73,
+        image: 'coffee-2.jpg',
+        country: 'Russia',
+      },
+      {
+        id: 2,
+        name: '',
+        price: 10.73,
+        image: 'coffee-3.jpg',
+        country: 'Brazilia',
+      },
+      {
+        id: 3,
+        name: '',
+        price: 10.73,
+        image: 'coffee-4.jpg',
+        country: 'Bustan',
+      },
+      {
+        id: 4,
+        name: '',
+        price: 10.73,
+        image: 'coffee-2.jpg',
+        country: 'Sibiria',
+      },
+      {
+        id: 5,
+        name: '',
+        price: 10.73,
+        image: 'coffee-1.jpg',
+        country: 'Abrikosia',
+      },
+    ],
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    getGoodsCoffee: (state) => state.goods, //Упрастим)) опять))))
+  },
+};
+export default pleasure;
--- src/views/ForYouPleasureView.vue
+++ src/views/ForYouPleasureView.vue
@@ -70,53 +70,10 @@ export default {
     NavBarComponent,
     ShopCard,
   },
-  data() {
-    return {
-      goods: [
-        {
-          id: 0,
-          name: '',
-          price: 10.73,
-          image: 'coffee-1.jpg',
-          country: 'Russia',
-        },
-        {
-          id: 1,
-          name: '',
-          price: 10.73,
-          image: 'coffee-2.jpg',
-          country: 'Russia',
-        },
-        {
-          id: 2,
-          name: '',
-          price: 10.73,
-          image: 'coffee-3.jpg',
-          country: 'Brazilia',
-        },
-        {
-          id: 3,
-          name: '',
-          price: 10.73,
-          image: 'coffee-4.jpg',
-          country: 'Bustan',
-        },
-        {
-          id: 4,
-          name: '',
-          price: 10.73,
-          image: 'coffee-2.jpg',
-          country: 'Sibiria',
-        },
-        {
-          id: 5,
-          name: '',
-          price: 10.73,
-          image: 'coffee-1.jpg',
-          country: 'Abrikosia',
-        },
-      ],
-    };
+  computed: {
+    goods() {
+      return this.$store.state['pleasure/getGoodsCoffee'];
+    },
   },
 };
 </script>
